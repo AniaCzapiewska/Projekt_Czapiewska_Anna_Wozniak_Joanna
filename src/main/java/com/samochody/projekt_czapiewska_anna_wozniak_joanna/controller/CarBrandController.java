7484b0109611edf59ab9fe06dc7914772e519594
annaczapiewska <ania.czapiewska@op.pl>
2025-01-16 16:35:09 +0100

    komentarz

--- src/main/java/com/samochody/projekt_czapiewska_anna_wozniak_joanna/controller/CarBrandController.java
+++ src/main/java/com/samochody/projekt_czapiewska_anna_wozniak_joanna/controller/CarBrandController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 public class CarBrandController {
 
     private final CarBrandService carBrandService;
-
+//cokolwiek
     public CarBrandController(CarBrandService carBrandService) {
         this.carBrandService = carBrandService;
     }
